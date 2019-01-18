89f06e9188ba7e0689e77e36bda207bbfb6892a2
AnasTheOne <40430115+AnasTheOne@users.noreply.github.com>
2019-01-18 20:30:36 +0100

    Add files via upload

---
+++ Marching/functions.cpp
@@ -0,0 +1,99 @@
+#include "prog.h"
+
+int* extract(string line){
+    std::string coord (line);
+    std::string::size_type sz1;
+    std::string::size_type sz2;
+    int x = std::stoi (coord,&sz1);
+    int y = std::stoi (coord.substr(sz1),&sz2);
+    int z = std::stoi (coord.substr(sz1+sz2));
+    int* res = (int*) malloc(sizeof(int)*3);
+    res[0] = x;
+    res[1] = y;
+    res[2] = z;
+    return res;
+}
+
+array<int,6> bords(vector<array<int,3>> Points){
+    int minX,minY,minZ,maxX,maxY,maxZ;
+    array<int,6> res;
+    auto point = Points.begin();
+    minX = (*point)[0]; minY = (*point)[1]; minZ = (*point)[2];
+    maxX = (*point)[0]; maxY = (*point)[1]; maxZ = (*point)[2];
+    ++point;
+    for (; point != Points.end(); ++point){
+        if ((*point)[0]<minX) minX = (*point)[0];
+        if ((*point)[0]>maxX) maxX = (*point)[0];
+
+        if ((*point)[1]<minY) minY = (*point)[1];
+        if ((*point)[1]>maxY) maxY = (*point)[1];
+
+        if ((*point)[2]<minZ) minZ = (*point)[2];
+        if ((*point)[2]>maxZ) maxZ = (*point)[2];
+    }
+    res[0] = minX;res[1] = minY;res[2] = minZ;
+    res[3] = maxX;res[4] = maxY;res[5] = maxZ;
+    //cout << res[0] << "\t" << res[3] << "\t" << res[1] << "\t" << res[4] << "\t" << res[2] << "\t" << res[5] << "\n";
+    return res;
+}
+
+array<int,4> bordsX(vector<array<int,3>> Points,int i){
+    int minY,minZ,maxY,maxZ;
+    array<int,4> res;
+    auto point = Points.begin();
+    minY = (*point)[1]; minZ = (*point)[2];
+    maxY = (*point)[1]; maxZ = (*point)[2];
+    ++point;
+    for (; point != Points.end(); ++point){
+        if ((*point)[0]==i){
+            if ((*point)[1]<minY) minY = (*point)[1];
+            if ((*point)[1]>maxY) maxY = (*point)[1];
+
+            if ((*point)[2]<minZ) minZ = (*point)[2];
+            if ((*point)[2]>maxZ) maxZ = (*point)[2];
+        }
+    }
+    res[0] = minY;res[1] = minZ;
+    res[2] = maxY;res[3] = maxZ;
+    //cout << res[0] << "\t" << res[3] << "\t" << res[1] << "\t" << res[4] << "\t" << res[2] << "\t" << res[5] << "\n";
+    return res;
+}
+
+array<int,2> bordsXY(vector<array<int,3>> Points,int i,int j){
+    int minZ,maxZ;
+    array<int,2> res;
+    auto point = Points.begin();
+    minZ = (*point)[2];
+    maxZ = (*point)[2];
+    ++point;
+    for (; point != Points.end(); ++point){
+        if ((*point)[0]==i && (*point)[1]==j ){
+
+            if ((*point)[2]<minZ) minZ = (*point)[2];
+            if ((*point)[2]>maxZ) maxZ = (*point)[2];
+        }
+    }
+    res[0] = minZ;
+    res[1] = maxZ;
+    //cout << res[0] << "\t" << res[3] << "\t" << res[1] << "\t" << res[4] << "\t" << res[2] << "\t" << res[5] << "\n";
+    return res;
+}
+
+bool cherche3(vector<array<int,3>> Points, array<int,3> target){
+    for(auto &point:Points){
+        if(point == target) return true;
+    }
+    return false;
+}
+
+bool cherche8(vector<array<int,8>> Points, array<int,8> target){
+    for(auto &point:Points){
+        if(point == target) return true;
+    }
+    return false;
+}
+
+RealPoint IntersPoint(array<int,3> P1,array<int,3> P2,double alpha){
+    return RealPoint(P1[0]+alpha*(P2[0]-P1[0]),P1[1]+alpha*(P2[1]-P1[1]),P1[2]+alpha*(P2[2]-P1[2]));
+}
+
---
+++ Marching/prog.h
@@ -0,0 +1,35 @@
+//
+// Created by anas on 11/01/19.
+//
+
+#ifndef MARCHINGCUBE_PROG_H
+#define MARCHINGCUBE_PROG_H
+#include "DGtal/helpers/StdDefs.h"
+#include "DGtal/base/Common.h"
+#include "DGtal/shapes/Mesh.h"
+#include "DGtal/shapes/MeshVoxelizer.h"
+#include "DGtal/io/boards/Board3D.h"
+#include "DGtal/io/writers/MeshWriter.h"
+#include "DGtal/io/readers/VolReader.h"
+#include "DGtal/io/Display3D.h"
+#include "DGtal/io/viewers/Viewer3D.h"
+#include "DGtal/io/DrawWithDisplay3DModifier.h"
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+using namespace DGtal;
+using namespace DGtal::Z3i;
+
+int* extract(string line);
+array<int,6> bords(vector<array<int,3>> Points);
+array<int,4> bordsX(vector<array<int,3>> Points,int i);
+array<int,2> bordsXY(vector<array<int,3>> Points,int i,int j);
+array<int,6> bordsIntegral(vector<array<int,3>> Points,int i,int j,int k);
+bool cherche3(vector<array<int,3>> Points, array<int,3> target);
+bool cherche8(vector<array<int,8>> Points, array<int,8> target);
+RealPoint IntersPoint(array<int,3> P1,array<int,3> P2,double alpha);
+
+#endif //MARCHINGCUBE_PROG_H
